ffce26fdf798688c7430d3bf260c57568c492044
Juho <juho.jokelainen@vincit.fi>
2017-10-11 14:55:05 +0300

    Fix ruuvitag base64 decoding
    Ruuvitag uses non-standard charset for base64 for url-safety.

--- app/src/main/java/com/bridou_n/beaconscanner/utils/copyPaste/Base64.java
+++ app/src/main/java/com/bridou_n/beaconscanner/utils/copyPaste/Base64.java
@@ -12,7 +12,7 @@ public class Base64 {
                 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
                 'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
                 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
-                'w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/' };
+                'w','x','y','z','0','1','2','3','4','5','6','7','8','9','-','_' };
 
         StringBuilder buffer = new StringBuilder();
         int pad = 0;
@@ -37,7 +37,7 @@ public class Base64 {
             }
         }
         for (int j = 0; j < pad; j++) {
-            buffer.append("=");
+            buffer.append(".");
         }
 
         return buffer.toString();
